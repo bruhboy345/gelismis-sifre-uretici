4481e5d3e905adca0f3c333f83fcca046ba38062
bruhboy345 <61841930+bruhboy345@users.noreply.github.com>
2021-01-28 00:17:52 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-**Lütfen projeyi çalmak vb. şeyler yapmayın MIT Lisansına uygun kullanın ve her şifre yenilediğinizde şifre.txt değişir yakında halledicem <3**
+**Lütfen projeyi çalmak vb. şeyler yapmayın MIT Lisansına uygun kullanın <3**
 
 Youtube Kanalım : [Tıkla](https://www.youtube.com/channel/UCOdKRNHVc49z5IDFPnzR1eA)
 Discord Sunucum : [Tıkla](https://discord.gg/3FwJQeW)
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fs = require('fs').promises;
 const şifre = uretici.generate({length: 30})
 async function openFile() {
     try {
-      await fs.writeFile('şifre.txt', `${şifre}`);
+      await fs.writeFile('./Şifreler/'+şifre+'.txt', şifre);
       console.log("Ahhh Tamam Şifre Mi İstedin? Tamam bruhboy345 Senin İçin 30 Rakamlık Bir Şifre Oluşturuyor.. Oluşturdu! Buyur:\n\n\n" + şifre)
     } catch (error) {
       console.error(`Oh Bir Hata Oluştur Sanırım Kontrol Etsen İyi Olucak: ${error.message}`);
